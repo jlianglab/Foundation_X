439b27addce8a06631d96db2f541810e7b24f68d
Nahid Ul Islam <zero.nahid.islam@gmail.com>
2024-11-25 12:58:57 -0700

    Add files via upload

---
+++ datasets/__init__.py
@@ -0,0 +1,55 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+import torch.utils.data
+import torchvision
+
+from .coco import build as build_coco
+
+
+def get_coco_api_from_dataset(dataset):
+    for _ in range(10):
+        # if isinstance(dataset, torchvision.datasets.CocoDetection):
+        #     break
+        if isinstance(dataset, torch.utils.data.Subset):
+            dataset = dataset.dataset
+    if isinstance(dataset, torchvision.datasets.CocoDetection):
+        return dataset.coco
+
+
+def build_dataset(image_set, args):
+    if args.dataset_file == 'coco':
+        return build_coco(image_set, args)
+    if args.dataset_file == 'chestxdetdataset':
+        return build_coco(image_set, args)
+    if args.dataset_file == 'vindrcxr_detect':
+        return build_coco(image_set, args)
+
+    if args.dataset_file == 'vindrcxr_OrganDetect':
+        return build_coco(image_set, args)
+
+    if args.dataset_file == 'vindrcxr_OrganDetectHL':
+        return build_coco(image_set, args)
+
+    if args.dataset_file == 'node21_noduleDataset':
+        return build_coco(image_set, args)
+
+    if args.dataset_file == 'tbx11k_catagnostic':
+        return build_coco(image_set, args)
+    
+    if args.dataset_file == 'foundation6_datasets':
+        return build_coco(image_set, args)
+    
+    if args.dataset_file == 'foundation6Ark6_datasets':
+        return build_coco(image_set, args)
+
+
+    if args.dataset_file == 'coco_panoptic':
+        # to avoid making panopticapi required for coco
+        from .coco_panoptic import build as build_coco_panoptic
+        return build_coco_panoptic(image_set, args)
+    if args.dataset_file == 'o365':
+        from .o365 import build_o365_combine
+        return build_o365_combine(image_set, args)
+    if args.dataset_file == 'vanke':
+        from .vanke import build_vanke
+        return build_vanke(image_set, args)
+    raise ValueError(f'dataset {args.dataset_file} not supported')
