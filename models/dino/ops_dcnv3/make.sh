28e6aaa2d0e70bcf83b29202e4ebd08baecb0db5
Nahid Ul Islam <zero.nahid.islam@gmail.com>
2024-11-25 12:54:30 -0700

    Add files via upload

---
+++ models/dino/ops_dcnv3/make.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# --------------------------------------------------------
+# InternImage
+# Copyright (c) 2022 OpenGVLab
+# Licensed under The MIT License [see LICENSE for details]
+# --------------------------------------------------------
+
+~/.conda/envs/tf-tnt-gpu/bin/python setup.py build install
