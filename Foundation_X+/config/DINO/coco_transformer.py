a27ed68e56261100121a2bd9ad5b574e74c5caa0
Nahid Ul Islam <zero.nahid.islam@gmail.com>
2026-03-24 16:39:30 -0700

    Add files via upload

---
+++ Foundation_X+/config/DINO/coco_transformer.py
@@ -0,0 +1,19 @@
+data_aug_scales = [480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800]
+data_aug_max_size = 1333
+# data_aug_max_size = 800
+
+# data_aug_scales = [480]
+# data_aug_max_size = 480
+
+# data_aug_scales = [224]
+# data_aug_max_size = 224
+
+data_aug_scales2_resize = [400, 500, 600]
+data_aug_scales2_crop = [384, 600]
+
+# data_aug_scales2_resize = [224]
+# data_aug_scales2_crop = [224]
+
+
+data_aug_scale_overlap = None
+
